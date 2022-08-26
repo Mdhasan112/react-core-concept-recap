8210cab86f0e1ed8c04735e8011320e788c9caec
hasan <mdhasanhk.98@gmail.com>
2022-08-26 23:12:03 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const nayoks = ['sakib', "milon", "reakib", "mushi"]
   return (
     <div className="App">
+      <MovieCounter></MovieCounter>
+        <Nayok name= {nayoks[0]} age ="50"></Nayok>
+        <Nayok name= {nayoks[1]}></Nayok>
+        <Nayok name= {nayoks[2]}></Nayok>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        
+        <ul>
+        {nayoks.map(nayok => <li>{nayok}</li>)}
+        </ul>
+        
       </header>
     </div>
   );
+}
+
+function MovieCounter() {
+  const[counter, setCounter] = useState(0)
+  const clickhandler = () => {
+    setCounter(counter + 1)
+  }
+  return (
+    <div>
+      <button onClick ={clickhandler}>add movie</button>
+      <h3>Number of counter: {counter}</h3>
+      <MovieDisplay>{counter}</MovieDisplay>
+    </div>
+  )
+}
+
+function MovieDisplay () {
+  return <h3>moive attech :</h3>
+}
+
+function Nayok (props) {
+  
+  const nayokStyle = {
+    border: "2px solid purple",
+    borderRedius: '10px',
+    padding: "20px",
+    margin: "20px"
+  }
+
+  return (
+    <div style = {nayokStyle}>
+      <h3>{props.name}</h3>
+      <h3>age: {props.age}</h3>
+    </div>
+  )
 }
 
 export default App;
